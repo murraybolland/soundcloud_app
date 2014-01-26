d6bea3b629b81049f6891aee938126f33fc38185
sh_sk <02luvsic@gmail.com>
2014-01-27 00:13:44 +0900

    no message

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,7 +4,8 @@ $(function(){
         SC.initialize({
             client_id: '3a426c57135b968be2dacd27418f725',
         });
-        SC.stream('/tracks/293', function(sound){
+        SC.stream('/tracks/126841935', function(sound){
+            console.log(sound);
             sound.play();
         });
     }
