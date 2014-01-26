9222e92c8cc559ed9e506052de30a1549fe6679a
sh_sk <02luvsic@gmail.com>
2014-01-27 00:16:55 +0900

    sound cloud play test

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,7 @@ $(function(){
     window.addEventListener('load', init);
     function init(){
         SC.initialize({
-            client_id: '3a426c57135b968be2dacd27418f725',
+            client_id: 'f3a426c57135b968be2dacd27418f725',
         });
         SC.stream('/tracks/126841935', function(sound){
             console.log(sound);
