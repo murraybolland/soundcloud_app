e8bb86c45801f96961e473b714002803867c0825
sh_sk <02luvsic@gmail.com>
2014-01-27 22:43:46 +0900

    last local

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,30 @@
 $(function(){
     window.addEventListener('load', init);
+    
+    var soundArray = [
+        '/tracks/130542777',
+        '/tracks/111551647'
+    ];
     function init(){
         SC.initialize({
             client_id: 'f3a426c57135b968be2dacd27418f725',
         });
-        SC.stream('/tracks/126841935', function(sound){
-            console.log(sound);
-            sound.play();
-        });
+        playNext();
+//        SC.stream(soundArray['1'], {
+//            autoPlay: true,
+//            ontimedcomments: function(comments){
+//                console.log(comments[0].body);
+//            }
+//        });
+    }
+    function playNext(){
+        var nextSound = soundArray.shift();
+        //playSound(nextSound);
+        setTrackInfo(nextSound);
+    }
+    function setTrackInfo(trackurl){
+        SC.get(trackurl, {limit: 1}, function(t){
+            console.log(t);
+        })
     }
 });
